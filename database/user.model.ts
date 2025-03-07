e4260097bed0dd967131e37ea9fadfe02675e9c1
Sambal Shrestha <splashofwater900@gmail.com>
2025-03-07 09:25:22 +0545

    sign-in/up backend implemented

--- database/user.model.ts
+++ database/user.model.ts
@@ -14,7 +14,7 @@ export interface IUser extends Document {
 }
 
 const UserSchema = new Schema({
-	uesrId: { type: String, required: true },
+	userId: { type: String, required: true },
 	name: { type: String, required: true },
 	username: { type: String, required: true, unique: true },
 	email: { type: String, required: true, unique: true },
